30e4dcc51a2006225d07149c4420b31aef2becbc
murthuja <murthuja1708@gmail.com>
2022-01-23 12:46:56 +0530

    getting run time stats

--- delay_tasks/main/tasks.c
+++ delay_tasks/main/tasks.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<string.h>
 #include<freertos/FreeRTOS.h>
 #include<freertos/task.h>
 
@@ -9,13 +10,17 @@ static int count=0;
 
 void Task1(void* parameters)
 {   
+    printf("from Task1\n");
+    char buffer[50]={'\0'};
     for(;;)
     {
-        count++;
         printf("%s from Task1\n",(char*)parameters);
+        count++;
         if(count%2 == 0)
         {
             vTaskDelay(pdMS_TO_TICKS(1000));
+            vTaskGetRunTimeStats(buffer);
+            memset(buffer,0,sizeof(buffer));
         }
     }
     vTaskDelete(NULL);
@@ -24,14 +29,18 @@ void Task1(void* parameters)
 
 void Task2(void* parameters)
 {
-    
+    printf("from Task2\n");
+    char buffer[50]={'\0'};
     for(;;)
     {
-        count++;
         printf("%s from Task2\n",(char*)parameters);
+        count++;
         if(count%2 == 0)
         {
             vTaskDelay(pdMS_TO_TICKS(1000));
+            vTaskGetRunTimeStats(buffer);
+            printf("%s\n",buffer);
+            memset(buffer,0,sizeof(buffer));
         }
     }
     vTaskDelete(NULL);
@@ -41,17 +50,8 @@ void Task2(void* parameters)
 void app_main()
 {
     BaseType_t result;
-    result=xTaskCreate(Task1,"task1",2048,"hello",1,&task1_handler);
-    if(result == pdPASS)
-    {
-        printf("task1 is created\n");
-    }
-    printf("creating task2\n");
+    result=xTaskCreate(Task1,"task1",2048,"hello",6,&task1_handler);
 
     result=xTaskCreate(Task2,"task2",2048,"bye",2,&task2_handler); 
-    if(result == pdPASS)
-    {
-        printf("task2 is created\n");
-    }
 
 }
