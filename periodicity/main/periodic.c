be0ac9d2fa9bc4a20bbee8d44756e6ba4c5d731a
murthuja <murthuja1708@gmail.com>
2022-01-23 12:44:02 +0530

    checking jitter

--- periodicity/main/periodic.c
+++ periodicity/main/periodic.c
@@ -4,36 +4,31 @@
 #include<driver/gpio.h>
 
 
-void SensorTask(void* parameters)
+void DummyTask3(void* parameters)
 {
-    //TickType_t xLastWakeTime;
-
-    //const TickType_t xFrequency=pdMS_TO_TICKS(500);
-    //xLastWakeTime = xTaskGetTickCount();
-
+	TickType_t xLastWakeTime;
     for ( ;;)
     {
         //vTaskDelayUntil(&xLastWakeTime,xFrequency);
-        printf("sensor task --> %d\n",xTaskGetTickCount());
+		xLastWakeTime=xTaskGetTickCount() * portTICK_PERIOD_MS;
+        printf("DummyTask3 --> %d\n",xLastWakeTime);
         vTaskDelay(pdMS_TO_TICKS(1000));
-        printf("sensor task delay done at --> %d\n",xTaskGetTickCount());
+        printf("Task3 done at  %d msecs\n",(xTaskGetTickCount() * portTICK_PERIOD_MS));
     }
     
 }
 
 void DummyTask1(void* parameters)
 {
-    //TickType_t xLastWakeTime;
-
-    //const TickType_t xFrequency=pdMS_TO_TICKS(500);
-    //xLastWakeTime = xTaskGetTickCount();
+	TickType_t xLastWakeTime;
 
     for ( ;;)
     {
         //vTaskDelayUntil(&xLastWakeTime,xFrequency);
-        printf("Dummy task1 --> %d\n",xTaskGetTickCount());
+        xLastWakeTime=xTaskGetTickCount() * portTICK_PERIOD_MS;
+        printf("Dummy task1 --> %d\n",xLastWakeTime);
         vTaskDelay(pdMS_TO_TICKS(500));
-        printf("Dummy task1 done at --> %d\n",xTaskGetTickCount());
+        printf("Task 1 done at %d msecs\n",(xTaskGetTickCount() * portTICK_PERIOD_MS));
     }
     
 }
@@ -41,18 +36,19 @@ void DummyTask1(void* parameters)
 
 void DummyTask2(void* parameters)
 {
-    //TickType_t xLastWakeTime;
+    TickType_t xLastWakeTime;
 
     //const TickType_t xFrequency=pdMS_TO_TICKS(500);
     //xLastWakeTime = xTaskGetTickCount();
-
+    
     for ( ;;)
     {
         
+		xLastWakeTime=xTaskGetTickCount() * portTICK_PERIOD_MS;
         //vTaskDelayUntil(&xLastWakeTime,xFrequency);
-        printf("Dummy task2 --> %d\n",xTaskGetTickCount());
+        printf("Dummy task2 --> %d\n",xLastWakeTime);
         vTaskDelay(pdMS_TO_TICKS(800));
-        printf("Dummy task2 done at --> %d\n",xTaskGetTickCount());
+        printf("Task2 done at %d msecs\n",(xTaskGetTickCount() * portTICK_PERIOD_MS));
     }
     
 }
@@ -60,21 +56,22 @@ void DummyTask2(void* parameters)
 void app_main()
 {
     BaseType_t result;
-    result=xTaskCreate(SensorTask,"Sensor Task",2048,NULL,2,NULL);
+    printf("portTICK_PERIOD_MS --> %d\n",portTICK_PERIOD_MS);
+    result=xTaskCreate(DummyTask1,"Dummy Task1",2048,NULL,1,NULL);
     if(result == pdPASS)
     {
         printf("task1 is created\n");
     }
 
-    result=xTaskCreate(DummyTask1,"Dummy Task1",2048,NULL,2,NULL);
+    result=xTaskCreate(DummyTask2,"Dummy Task2",2048,NULL,1,NULL);
     if(result == pdPASS)
     {
-        printf("Dummy Task1 is created\n");
+        printf("Task2 is created\n");
     }
 
-    result=xTaskCreate(DummyTask2,"Dummy Task1",2048,NULL,2,NULL);
+    result=xTaskCreate(DummyTask3,"Dummy Task3",2048,NULL,2,NULL);
     if(result == pdPASS)
     {
-        printf("Dummy Task2 is created\n");
+        printf("Task3 is created\n");
     }
 }
