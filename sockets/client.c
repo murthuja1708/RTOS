644eedee244807894f3cd69a286d746011045134
murthuja <murthuja1708@gmail.com>
2022-01-23 12:32:40 +0530

    client

---
+++ sockets/client.c
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include<linux/in.h>
+#include<errno.h>
+#include<string.h>
+#include<unistd.h>
+
+int main()
+{
+	int cfd,status;
+	struct sockaddr_in caddr;
+
+	ssize_t bytes_sent,bytes_received;
+
+	char received[100];
+
+	cfd=socket(AF_INET,SOCK_STREAM,0);
+
+	caddr.sin_family = AF_INET;
+	caddr.sin_addr.s_addr = inet_addr("192.168.1.18");
+	caddr.sin_port = htons(1234);
+	
+	status = connect(cfd,(struct sockaddr*)&caddr,sizeof(struct sockaddr_in));
+	if(status == -1)
+	{
+		perror("error while connecting\n");
+		return -1;
+	}
+
+	char* str="Hello server this is client";
+
+	bytes_sent = send(cfd,str,strlen(str),0);
+
+	if(bytes_sent == -1)
+	{
+		perror("error while connecting\n");
+		return -1;
+	}
+		
+	bytes_received = recv(cfd,received,100,0);
+
+	if(bytes_received == -1)
+	{
+		perror("error while receiving data\n");
+		return -1;
+	}
+	
+	printf("%s\n",received);
+
+	while(1);
+	close(cfd);
+
+	return 0;	
+
+}
