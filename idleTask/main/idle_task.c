3b2fea5c9edb8e38085cc865a22ecb60654045a0
murthuja <murthuja1708@gmail.com>
2022-01-24 20:16:09 +0530

    registering for callback function

--- idleTask/main/idle_task.c
+++ idleTask/main/idle_task.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 #include<freertos/FreeRTOS.h>
 #include<freertos/task.h>
+#include<esp_freertos_hooks.h>
+#include<esp_sleep.h>
+
 
 TaskHandle_t task1_handler;
 TaskStatus_t task_status;
@@ -9,17 +12,23 @@ volatile uint32_t count=0;
 
 //void vApplicationIdleHook( void );
 
-void vApplicationIdleHook(void){
-    count++;
+bool IdleTask(void){
+	count++;
+
+	esp_light_sleep_start(); //not getting wakeup
+	return true;
 }
 
 void task1(void* parameters)
 {
 	const TickType_t delay=pdMS_TO_TICKS(1000);
+	char buffer[50]={'\0'};
 
 	for(;;)
 	{
 		printf("task1 delaying to 1sec with count %d\n",count);
+		vTaskGetRunTimeStats(buffer);
+		printf("%s\n",buffer);
 		vTaskDelay(delay);
 	}
 }
@@ -28,7 +37,11 @@ void task1(void* parameters)
 void app_main()
 {
     BaseType_t result;
-    
+	esp_register_freertos_idle_hook(IdleTask);
     result=xTaskCreate(task1,"task1",2048,NULL,3,&task1_handler);
+	if(result == pdTRUE)
+	{
+		printf("task created\n");
+	}
 
 }
